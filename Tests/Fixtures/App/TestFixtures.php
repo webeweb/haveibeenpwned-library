00be5ba6d23fa1dd5402cce4b9c8bcdf6212e48b
webeweb <support@webeweb.fr>
2018-08-20 18:49:00 +0200

    Add SAMPLE_DATA_CLASS_RESPONSE and SAMPLE_RANGE_RESPONSE constants

--- Tests/Fixtures/App/TestFixtures.php
+++ Tests/Fixtures/App/TestFixtures.php
@@ -63,6 +63,15 @@ final class TestFixtures {
 EOTXT;
 
     /**
+     * Sample data classes response.
+     *
+     * @var string
+     */
+    const SAMPLE_DATA_CLASS_RESPONSE = <<< 'EOTXT'
+["Email addresses", "Password hints", "Passwords", "Usernames"]
+EOTXT;
+
+    /**
      * Sample paste response.
      *
      * @var string
@@ -83,6 +92,19 @@ EOTXT;
 "EmailCount":30
 }
 ]
+EOTXT;
+
+    /**
+     * Sample range response.
+     *
+     * @var string
+     */
+    const SAMPLE_RANGE_RESPONSE = <<< 'EOTXT'
+0018A45C4D1DEF81644B54AB7F969B88D65:1
+00D4F6E8FA6EECAD2A3AA415EEC418D38EC:2
+011053FD0102E94D6AE2F8B83D76FAF94F6:1
+012A7CA357541F0AC487871FEEC1891C49C:2
+0136E006E24E7D152139815FB0FC6A50B15:2
 EOTXT;
 
     /**
