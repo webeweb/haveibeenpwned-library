7c9f870ee429b22f56ea6e32840e33717704d261
webeweb <support@webeweb.fr>
2018-09-15 14:42:22 +0200

    Add an abstract HaveIBeenPwned framework test case

---
+++ tests/Cases/AbstractHaveIBeenPwnedFrameworkTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Tests\Cases;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Abstract HaveIBeenPwned framework test case.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Tests\Cases
+ * @abstract
+ */
+abstract class AbstractHaveIBeenPwnedFrameworkTestCase extends TestCase {
+
+}
