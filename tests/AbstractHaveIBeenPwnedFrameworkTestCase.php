3785ffaf7182446214fa991a932ff870f1c333bd
webeweb <support@webeweb.fr>
2018-09-17 10:03:44 +0200

    Move abstract HaveIBeenPwned framework test case

--- tests/AbstractHaveIBeenPwnedFrameworkTestCase.php
+++ tests/AbstractHaveIBeenPwnedFrameworkTestCase.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\HaveIBeenPwned\Tests\Cases;
+namespace WBW\Library\HaveIBeenPwned\Tests;
 
 use PHPUnit\Framework\TestCase;
 
