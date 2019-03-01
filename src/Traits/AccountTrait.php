5706f55ce6c97b2d124db2dd736caceb7e7c4d71
webeweb <support@webeweb.fr>
2019-03-01 18:12:04 +0100

    Add account trait

---
+++ src/Traits/AccountTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Traits;
+
+/**
+ * Account trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Traits
+ */
+trait AccountTrait {
+
+    /**
+     * Account
+     *
+     * @var string
+     */
+    private $account;
+
+    /**
+     * Get the account.
+     *
+     * @return string Returns the account.
+     */
+    public function getAccount() {
+        return $this->account;
+    }
+
+    /**
+     * Set the account.
+     *
+     * @param string $account The account.
+     */
+    public function setAccount($account) {
+        $this->account = $account;
+        return $this;
+    }
+}
---
+++ tests/Traits/AccountTraitTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Tests\Traits;
+
+use WBW\Library\HaveIBeenPwned\Tests\AbstractTestCase;
+use WBW\Library\HaveIBeenPwned\Tests\Fixtures\Traits\TestAccountTrait;
+
+/**
+ * Account trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Tests\Traits
+ */
+class AccountTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestAccountTrait();
+
+        $this->assertNull($obj->getAccount());
+    }
+
+    /**
+     * Tests the setAccount() method.
+     *
+     * @return void
+     */
+    public function testSetAccount() {
+
+        $obj = new TestAccountTrait();
+
+        $obj->setAccount("account");
+        $this->assertEquals("account", $obj->getAccount());
+    }
+}
