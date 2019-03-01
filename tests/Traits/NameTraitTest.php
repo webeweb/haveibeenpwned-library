1ef85203fb4f36f12deacb2abd70565a85f3dce8
webeweb <support@webeweb.fr>
2019-03-01 18:14:26 +0100

    Add name trait

---
+++ tests/Traits/NameTraitTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Tests\Traits;
+
+use WBW\Library\HaveIBeenPwned\Tests\AbstractTestCase;
+use WBW\Library\HaveIBeenPwned\Tests\Fixtures\Traits\TestNameTrait;
+
+/**
+ * Name trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Tests\Traits
+ */
+class NameTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new TestNameTrait();
+
+        $this->assertNull($obj->getName());
+    }
+
+    /**
+     * Tests the setName() method.
+     *
+     * @return void
+     */
+    public function testSetName() {
+
+        $obj = new TestNameTrait();
+
+        $obj->setName("name");
+        $this->assertEquals("name", $obj->getName());
+    }
+}
