26e51f238ffd2c2a91f004bb9159058918d7b404
webeweb <support@webeweb.fr>
2019-03-01 18:17:56 +0100

    Rename Response interface

--- src/API/ResponseInterface.php
+++ src/API/ResponseInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the haveibeenpwned-library package.
  *
- * (c) 2018 WEBEWEB
+ * (c) 2019 WEBEWEB
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,39 +12,38 @@
 namespace WBW\Library\HaveIBeenPwned\API;
 
 /**
- * HaveIBeenPwned model interface.
+ * Response interface.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Library\HaveIBeenPwned\API
  */
-interface HaveIBeenPwnedModelInterface {
+interface ResponseInterface {
 
     /**
-     * Date/time format "Added".
+     * Date/time format "added".
      *
      * @var string
      */
     const DATETIME_FORMAT_ADDED = "Y-m-d\TH:i\Z";
 
     /**
-     * Date/time format "Breach".
+     * Date/time format "breach".
      *
      * @var string
      */
     const DATETIME_FORMAT_BREACH = "Y-m-d";
 
     /**
-     * Date/time format "Date".
+     * Date/time format "date".
      *
      * @var string
      */
     const DATETIME_FORMAT_DATE = "Y-m-d\TH:i:s\Z";
 
     /**
-     * Date/time format "Modified".
+     * Date/time format "modified".
      *
      * @var string
      */
     const DATETIME_FORMAT_MODIFIED = "Y-m-d\TH:i\Z";
-
 }
---
+++ tests/API/ResponseInterfaceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Tests\API;
+
+use WBW\Library\HaveIBeenPwned\API\ResponseInterface;
+use WBW\Library\HaveIBeenPwned\Tests\AbstractTestCase;
+
+/**
+ * Response interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Tests\API
+ */
+class ResponseInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("Y-m-d\TH:i\Z", ResponseInterface::DATETIME_FORMAT_ADDED);
+        $this->assertEquals("Y-m-d", ResponseInterface::DATETIME_FORMAT_BREACH);
+        $this->assertEquals("Y-m-d\TH:i:s\Z", ResponseInterface::DATETIME_FORMAT_DATE);
+        $this->assertEquals("Y-m-d\TH:i\Z", ResponseInterface::DATETIME_FORMAT_MODIFIED);
+    }
+}
