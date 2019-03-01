63ea86b543746f948dd74312cbf1a5bf88abb88c
webeweb <support@webeweb.fr>
2019-03-01 18:21:21 +0100

    Rename HaveIBeenPwned data class

---
+++ src/Model/DataClass.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Model;
+
+use WBW\Library\HaveIBeenPwned\Traits\NameTrait;
+
+/**
+ * Data class.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Model
+ */
+class DataClass {
+
+    use NameTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+}
---
+++ tests/Model/DataClassTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Tests\Model;
+
+use WBW\Library\HaveIBeenPwned\Model\DataClass;
+use WBW\Library\HaveIBeenPwned\Tests\AbstractTestCase;
+
+/**
+ * Data class test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Tests\Model
+ */
+class DataClassTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DataClass();
+
+        $this->assertNull($obj->getName());
+    }
+}
