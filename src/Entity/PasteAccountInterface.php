2543a165b4bb1bbbf22c7afd360a99a273bc8378
webeweb <support@webeweb.fr>
2019-05-13 21:47:16 +0200

    Add Paste account interface

---
+++ src/Entity/PasteAccountInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Entity;
+
+/**
+ * Paste account interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Entity
+ */
+interface PasteAccountInterface extends HaveIBeenPwnedEntityInterface {
+
+    /**
+     * Get the account.
+     *
+     * @return string Returns the account.
+     */
+    public function getHaveIBeenPwnedAccount();
+}
