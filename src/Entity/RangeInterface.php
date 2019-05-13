591e1e2fb20468f79b3e7c585ae9cd82bf79fa49
webeweb <support@webeweb.fr>
2019-05-13 21:47:29 +0200

    Add Range interface

---
+++ src/Entity/RangeInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Entity;
+
+/**
+ * Range interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Entity
+ */
+interface RangeInterface extends HaveIBeenPwnedEntityInterface {
+
+    /**
+     * Get the hash.
+     *
+     * @return string Returns the hash.
+     */
+    public function getHaveIBeenPwnedHash();
+}
