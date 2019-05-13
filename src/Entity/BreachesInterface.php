9c7bee505e2d924f8bd1781fdf3a5ba3488a9c26
webeweb <support@webeweb.fr>
2019-05-13 21:46:54 +0200

    Add Breaches interface

---
+++ src/Entity/BreachesInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\HaveIBeenPwned\Entity;
+/**
+ * Breaches interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\HaveIBeenPwned\Entity
+ */
+interface BreachesInterface extends HaveIBeenPwnedEntityInterface {
+
+    /**
+     * Get the domain.
+     *
+     * @return string Returns teh domain.
+     */
+    public function getHaveIBeenPwnedDomain();
+}
